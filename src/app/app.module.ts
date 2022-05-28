ab56c3d2e756d820cc6f42a27232f6ee1b14335c
petyo_mitkov <petyo.mitkov@gmail.com>
2022-05-28 16:06:43 +0300

    Add navigation, layout and empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template-forms',
+    component: TemplateFormsComponent,
+  },
+  {
+    path: 'reactive-forms',
+    component: ReactiveFormsComponent,
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TemplateFormsComponent, ReactiveFormsComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes), NgbModule],
   providers: [],
   bootstrap: [AppComponent],
